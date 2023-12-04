c75080ffa476d6edc354384f46f188e519301329
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-12-04 15:07:06 +0000

    add clean-html and svelte things needed; some extra styles to help migration

---
+++ src/functions/clean-html/processHTML.ts
@@ -0,0 +1,90 @@
+import sanitizeHtml from "sanitize-html";
+import beautify from "js-beautify";
+
+const sanitise = (text: string) => {
+	const unwantedTags = ["span", "div"]; // Add other unwanted tags to this array
+
+	const sanitised = sanitizeHtml(text, {
+		allowedTags: sanitizeHtml.defaults.allowedTags.filter(
+			(tag) => !unwantedTags.includes(tag)
+		),
+		allowedAttributes: {
+			...sanitizeHtml.defaults.allowedAttributes,
+			a: ["href"],
+		},
+		transformTags: {
+			b: "strong",
+			span: (tagName, attribs) => {
+				const style = attribs.style || "";
+				if (style.includes("font-weight:700")) {
+					return {
+						tagName: "strong",
+						attribs: {},
+					};
+				}
+				if (style.includes("font-style:italic")) {
+					return {
+						tagName: "em",
+						attribs: {},
+					};
+				}
+				if (style.includes("text-decoration:underline")) {
+					return {
+						tagName: "u",
+						attribs: {},
+					};
+				}
+				return {
+					tagName: "",
+					attribs: {},
+				};
+			},
+		},
+		textFilter: (text) => {
+			return text.replace(/\s+/g, " ");
+		},
+		exclusiveFilter: (frame) => {
+			if (!frame.text.trim()) {
+				return true;
+			}
+			frame.text = frame.text.replace(/\s+/g, " ").trim();
+			return false;
+		},
+	});
+
+	return sanitised;
+};
+
+const cleanupGoogleRedirectURLs = (text: string): string => {
+	const anchorTagRegex = /<a [^>]*href="([^"]*)"[^>]*>(.*?)<\/a>/g;
+	return text.replace(anchorTagRegex, (_match, href, linkText) => {
+		// Extract the original URL from Google's wrapped URL
+		const originalURLMatch = href.match(/url\?q=([^&]*)/);
+		const originalURL = originalURLMatch
+			? decodeURIComponent(originalURLMatch[1])
+			: href;
+
+		return `<a href="${originalURL}">${linkText}</a>`;
+	});
+};
+
+export const prettify = (text: string): string => {
+	return beautify.html(text, {
+		max_preserve_newlines: 0,
+		preserve_newlines: false,
+	});
+};
+
+const trim = (text: string): string => {
+	return text.trim();
+};
+
+export const processHTML = (rawHTML: string): string => {
+	const transformers = [sanitise, cleanupGoogleRedirectURLs, prettify, trim];
+
+	let result: string = rawHTML;
+	for (const transformer of transformers) {
+		result = transformer(result);
+	}
+	return result;
+};
---
+++ src/islands/clean-html.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { shortcut } from "@svelte-put/shortcut";
+	import { processHTML } from "@/functions/clean-html/processHTML";
+	import Clipboard from "@/assets/images/clipboard.svg";
+	import ClipboardSuccessful from "@/assets/images/clipboardSuccessful.svg";
+
+	let unprocessedHTML = "";
+	let processedHTML = "";
+	let cleanHTMLCopied = false;
+
+	const handleDirtyHTMLSubmit = (): void => {
+		processedHTML = processHTML(unprocessedHTML);
+	};
+
+	const copyCleanHTMLToClipboard = (): void => {
+		navigator.clipboard.writeText(processedHTML);
+		cleanHTMLCopied = true;
+	};
+
+	const resetForm = (): void => {
+		processedHTML = "";
+		unprocessedHTML = "";
+		cleanHTMLCopied = false;
+	};
+
+	const handleCmdEnter = (): void => {
+		if (processedHTML !== "") return; // only run on dirty HTML view
+		handleDirtyHTMLSubmit();
+	};
+
+	const handleCmdC = (): void => {
+		if (processedHTML === "") return; // only run on clean HTML view
+		copyCleanHTMLToClipboard();
+	};
+
+	const handleCmdN = (): void => {
+		if (processedHTML === "") return; // only run on clean HTML view
+		resetForm();
+	};
+</script>
+
+<svelte:window
+	use:shortcut={{
+		trigger: {
+			key: "Enter",
+			modifier: ["ctrl", "meta"],
+			callback: handleCmdEnter,
+		},
+	}}
+/>
+{#if processedHTML === ""}
+	<section>
+		<div id="dirtyHTMLForm">
+			<div>
+				<label for="rawHTML"> Enter the HTML you want to clean</label>
+				<textarea
+					name="rawHTML"
+					id="rawHTML"
+					rows="10"
+					placeholder="Enter HTML here"
+					required
+					bind:value={unprocessedHTML}
+					autofocus
+				/>
+			</div>
+
+			<div class="flex items-center">
+				<button
+					class="button"
+					on:click|preventDefault={handleDirtyHTMLSubmit}
+					type="button">Clean →</button
+				>
+			</div>
+		</div>
+	</section>
+{/if}
+
+{#if processedHTML !== ""}
+	<section>
+		<div>
+			<label for="cleanHTML"> Here's the clean HTML </label>
+			<textarea
+				disabled
+				id="cleanHTML">{processedHTML}</textarea
+			>
+		</div>
+		<div class="flex flex-col gap-y-4">
+			<div>
+				<button
+					class="button small"
+					disabled={cleanHTMLCopied}
+					on:click={() => {
+						copyCleanHTMLToClipboard();
+					}}
+				>
+					<span>
+						{#if cleanHTMLCopied === false}
+							Copy clean HTML to clipboard
+						{:else}
+							Copied!
+						{/if}
+					</span>
+					<!-- <span
+							class:inline={!cleanHTMLCopied}
+							class:hidden={cleanHTMLCopied}
+						>
+							<img
+								src={Clipboard.src}
+								alt="clipboard"
+							/>
+						</span> -->
+					<!-- <span
+							class:inline={cleanHTMLCopied}
+							class:hidden={!cleanHTMLCopied}
+						>
+							<img
+								src={ClipboardSuccessful.src}
+								alt="clipboard with a tick"
+							/>
+						</span> -->
+				</button>
+			</div>
+
+			<div>
+				<button
+					class="button small"
+					on:click={() => {
+						resetForm();
+					}}
+				>
+					← Start again
+				</button>
+			</div>
+		</div>
+		<details class="mt-6">
+			<summary> See the original HTML </summary>
+
+			<div class="mt-6">
+				<label for="dirtyHTML"> Here's the original HTML </label>
+				<div id="dirtyHTML">
+					{unprocessedHTML}
+				</div>
+			</div>
+		</details>
+	</section>
+{/if}
