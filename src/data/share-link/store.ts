c8e3a79b7eefd572a2b14f2bfd81112d7f611945
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-12-08 15:59:24 +0000

    added twitter and email customisation

--- src/data/share-link/store.ts
+++ src/data/share-link/store.ts
@@ -8,31 +8,128 @@ export type ShareTargets =
 	| "Facebook Messenger"
 	| "Email";
 
-type ShareLinkSchemaType = {
-	[key in ShareTargets]: string;
-};
-
-// const shareLinkSchema: ShareLinkSchemaType = {
-// 	Facebook: "https://www.facebook.com/sharer/sharer.php?u=",
-// 	Twitter: "https://twitter.com/intent/tweet?text=",
-// 	WhatsApp: "whatsapp://send?text=",
-// 	"Facebook Messenger": "fb-messenger://share?link=",
-// 	Email: "mailto://",
-// };
-
 export const LinkToShare = writable("");
 
-const encodedLinkToShare = derived(LinkToShare, (LinkToShare: string) =>
-	encodeURIComponent(LinkToShare)
+const encodedLinkToShare: Readable<string> = derived(
+	LinkToShare,
+	(LinkToShare: string) => encodeURIComponent(LinkToShare)
 );
 
+type TwitterParametersType = {
+	text: string;
+	hashtags: string[];
+};
+
+function createTwitterParameters() {
+	const { subscribe, update, set } = writable<TwitterParametersType>({
+		text: "",
+		hashtags: [],
+	});
+
+	return {
+		subscribe,
+		set,
+		update,
+		setHashtags: (hashtags: string[]) =>
+			update((storeValue) => ({
+				...storeValue,
+				hashtags: hashtags.map((hashtag) => hashtag.replace(/\s/g, "_")), // replace spaces
+			})),
+	};
+}
+
+export const TwitterParameters = createTwitterParameters();
+
+// Writable store for Tags component to bind to
+export const InputHashtags = writable<string[]>([]);
+
+// Update TwitterParameters when the InputHashtags change
+InputHashtags.subscribe((hashtags) => {
+	TwitterParameters.setHashtags(hashtags);
+});
+
+export const twitterParametersPartOfURL: Readable<string> = derived(
+	TwitterParameters,
+	({ text, hashtags }: TwitterParametersType) => {
+		let parametersURL = "";
+		if (text !== "") {
+			parametersURL += `&text=${encodeURIComponent(text)}`;
+		}
+
+		if (hashtags.length > 0) {
+			parametersURL += `&hashtags=${hashtags.join(",")}`;
+		}
+		return parametersURL;
+	}
+);
+
+type EmailParametersType = {
+	subject: string;
+	body: string;
+};
+
+export const EmailParameters = writable<EmailParametersType>({
+	subject: "",
+	body: "",
+});
+
+// LinkToShare.subscribe((value) => {
+// 	EmailParameters.update((params) => ({ ...params, body: `\n\n${value}` }));
+// });
+
+export const emailParametersPartOfURL: Readable<string> = derived(
+	[EmailParameters, encodedLinkToShare],
+	([params, encodedLinkToShare]: [EmailParametersType, string]) => {
+		let parametersURL = "";
+
+		if (params.subject !== "") {
+			parametersURL += `&subject=${encodeURIComponent(params.subject)}`;
+		}
+
+		const bodyContent =
+			params.body !== ""
+				? `${params.body}\n\n${encodedLinkToShare}`
+				: `${encodedLinkToShare}`;
+
+		parametersURL += `&body=${encodeURIComponent(bodyContent)}`;
+
+		return parametersURL;
+	}
+);
+
+type ShareLinkType = {
+	platform: ShareTargets;
+	shareLink: string;
+};
+
+type ShareLinkSchemaType = ShareLinkType[];
+
 export const allShareLinks: Readable<ShareLinkSchemaType> = derived(
-	encodedLinkToShare,
-	(encodedLinkToShare: string) => ({
-		Facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedLinkToShare}`,
-		Twitter: `https://twitter.com/intent/tweet?text=${encodedLinkToShare}`,
-		WhatsApp: `whatsapp://send?text=${encodedLinkToShare}`,
-		"Facebook Messenger": `fb-messenger://share?link=${encodedLinkToShare}`,
-		Email: `mailto:?body=${encodedLinkToShare}`,
-	})
+	[encodedLinkToShare, twitterParametersPartOfURL, emailParametersPartOfURL],
+	([encodedLinkToShare, twitterParametersPartOfURL, emailParametersPartOfURL]: [
+		string,
+		string,
+		string
+	]) => [
+		{
+			platform: "Facebook",
+			shareLink: `https://www.facebook.com/sharer/sharer.php?u=${encodedLinkToShare}`,
+		},
+		{
+			platform: "Twitter",
+			shareLink: `https://twitter.com/intent/tweet?url=${encodedLinkToShare}${twitterParametersPartOfURL}`,
+		},
+		{
+			platform: "WhatsApp",
+			shareLink: `whatsapp://send?text=${encodedLinkToShare}`,
+		},
+		{
+			platform: "Facebook Messenger",
+			shareLink: `fb-messenger://share?link=${encodedLinkToShare}`,
+		},
+		{
+			platform: "Email",
+			shareLink: `mailto:${emailParametersPartOfURL}`,
+		},
+	]
 );
--- src/islands/share-link.svelte
+++ src/islands/share-link.svelte
@@ -2,7 +2,15 @@
 	import { getURLSearchParameter } from "@/functions/getURLSearchParameter";
 	import { isURL } from "@/functions/isURL";
 	import { onMount } from "svelte";
-	import { LinkToShare, allShareLinks } from "@/data/share-link/store";
+	import {
+		LinkToShare,
+		allShareLinks,
+		TwitterParameters,
+		EmailParameters,
+		InputHashtags,
+	} from "@/data/share-link/store";
+
+	import Tags from "svelte-tags-input";
 
 	// For testing 👇
 	onMount(() => {
@@ -48,34 +56,121 @@
 	<section class="mt-6">
 		<p class="h5 mb-0">Here are the share links</p>
 
-		<div id="prefillLinkWrapper">
-			<!-- <EmailProviderTabs /> -->
+		<div id="shareLinksWrapper">
+			{#each $allShareLinks as { platform, shareLink }}
+				<div>
+					<p class="mb-0"><strong>{platform}</strong></p>
+					<p>{shareLink}</p>
 
-			{#each Object.entries($allShareLinks) as [SharePlatform, ShareLink]}
-				<p><strong>{SharePlatform}</strong></p>
-				<p>{ShareLink}</p>
+					{#if platform === "Twitter"}
+						<label>
+							<small
+								>Template text: {$TwitterParameters.text.length} characters</small
+							>
+							<textarea
+								maxlength="280"
+								rows="3"
+								bind:value={$TwitterParameters.text}
+							></textarea>
+						</label>
+						<div class="hashtagTags">
+							<label for="svelte-tags-input">
+								<small>Type any hashtags (Enter to add a hashtag)</small>
+							</label>
+							<Tags
+								bind:tags={$InputHashtags}
+								placeholder={"Enter any hashtags for your Twitter share..."}
+								onlyUnique={true}
+							/>
+						</div>
+					{/if}
+
+					{#if platform === "Email"}
+						<label>
+							<small>Subject</small>
+							<input
+								type="text"
+								bind:value={$EmailParameters.subject}
+							/>
+						</label>
+						<label>
+							<small>Body (Link already added)</small>
+							<textarea bind:value={$EmailParameters.body}></textarea>
+						</label>
+					{/if}
+				</div>
 			{/each}
 		</div>
 	</section>
 {/if}
 
 <style>
-	#prefillLinkWrapper {
+	.hashtagTags :global(div.svelte-tags-input-layout) {
+		border: none;
+		padding: 0;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.hashtagTags :global(div.svelte-tags-input-layout:hover) {
+		border: none;
+	}
+	.hashtagTags :global(div.svelte-tags-input-layout:focus-within) {
+		outline: none;
+	}
+	.hashtagTags :global(input.svelte-tags-input) {
+		flex: 1 0 100%;
+		height: auto;
+		font-size: 100%;
+		font-family: ingra, sans-serif;
+		border-radius: 0;
+		border: 2px solid #2f2f2f;
+		margin-bottom: 0.75rem;
+		padding: 0.625rem;
+	}
+
+	.hashtagTags :global(input.svelte-tags-input::placeholder) {
+		font-weight: 700;
+	}
+
+	.hashtagTags :global(input.svelte-tags-input:focus) {
+		border: 2px solid #90c92a;
+		-webkit-box-shadow: 0 0 2px #b8e16e;
+		box-shadow: 0 0 2px #b8e16e;
+	}
+	.hashtagTags :global(div.svelte-tags-input-layout label) {
+		font-size: 80%;
+		font-weight: 700;
+		font-family: inherit;
+	}
+
+	.hashtagTags :global(button.svelte-tags-input-tag) {
+		display: flex;
+		align-items: center;
+		padding: 0.5rem 1rem;
+		background-color: #90c92a;
+		font-family: inherit;
+		font-size: 80%;
+		font-weight: 700;
+	}
+
+	.hashtagTags
+		:global(button.svelte-tags-input-tag span.svelte-tags-input-tag-remove) {
+		margin: 0;
+		padding-left: 1rem;
+		font-size: 22px;
+	}
+
+	#shareLinksWrapper {
+		display: flex;
+		flex-direction: column;
+		row-gap: 2rem;
 		margin-top: 0.5rem;
 		border: 1px solid #495057;
 		border-radius: 5px;
 		padding: 0.25rem 0.25rem;
 	}
 
-	#prefillLinkWrapper p {
-		padding: 1rem;
+	#shareLinksWrapper p {
 		word-break: break-words;
-		line-height: 1.75;
-	}
-	#prefillLinkWrapper p span {
-		font-weight: 400;
-		text-decoration: underline;
-		text-underline-offset: 8px;
-		text-decoration-thickness: 2px;
 	}
 </style>
