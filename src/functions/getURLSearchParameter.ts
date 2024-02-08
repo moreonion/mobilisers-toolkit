4c6fcaa963ca6c51c2850a219a102f6a3aa1adf6
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2024-02-08 11:49:41 +0000

    extract out param properly from url

--- src/functions/getURLSearchParameter.ts
+++ src/functions/getURLSearchParameter.ts
@@ -1,7 +1,16 @@
-export function getURLSearchParameter(name: string) {
-	const urlParams = new URLSearchParams(window.location.search);
+export function getURLSearchParameter(
+	name: string,
+	url: string = window.location.href
+): string | null {
+	// Find the first occurrence of '?' and extract the substring from there onwards
+	const queryStringStart = url.indexOf("?");
+	const queryString =
+		queryStringStart !== -1 ? url.substring(queryStringStart + 1) : "";
+	const urlParams = new URLSearchParams(queryString);
+
+	// Get the parameter by name, which is expected to be a URL
 	const paramValue = urlParams.get(name);
 
-	// If paramValue is not null, decode and return, otherwise return null
+	// Decode the extracted parameter value if it's not null
 	return paramValue !== null ? decodeURIComponent(paramValue) : null;
 }
